f2b1bb3c70183188cf1cd2d659b2fda1f18ba8cf
Mbonu Chinedu <cboy.chinedu@gmail.com>
2021-03-02 20:55:50 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// Importing the required routes 
+const path = require('path'); 
+const express = require('express'); 
+const config = require('config'); 
+const morgan = require('morgan'); 
+const mongodb = require('mongoose'); 
+const { ObjectId } = require('mongodb'); 
+
+// Getting the root path and exporting it 
+let rootPath = path.join(__dirname); 
+module.exports.rootPath = rootPath; 
+
+// Importing the required routes 
+const home = require('./routes/index.js'); 
+
+// Building the express application 
+const app = express(); 
+
+// Setting the views 
+app.set(
+    'view engine', 'pug'
+); 
+app.set(
+    'views', './views'
+); 
+
+// Adding the necessary middlewares 
+app.use(express.json()); 
+app.use(express.static('static')); 
+app.use(express.static('./static/css')); 
+app.use(express.static('./static/javascript')); 
+app.use(express.static('./static/templates')); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(morgan('tiny')); 
+
+// Configuring the routes 
+app.use('/', home); 
+
+// Setting the PORT and HOST connections 
+const PORT = process.env.PORT || 3000; 
+const HOST = process.env.HOST || 'localhost'; 
+
+// Running the server 
+app.listen(PORT, HOST, () =>
+{
+    // Displaying the server message 
+    console.log(`The server is running on ${HOST + ':' + PORT}`); 
+})
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+// Importing the necessary modules 
+const express = require('express'); 
+const path = require('path'); 
+const { rootPath } = require('../app.js'); 
+const router = express.Router(); 
+
+// Setting the routes for the home page 
+router.get('/', (req, res) =>
+{
+    // Getting the full path to the "index.html" file 
+    let fullPath = path.join(rootPath, 'static', 'templates', 'index.html'); 
+
+    // Sending the index page "index.html" 
+    console.log(fullPath); 
+    res.sendFile(fullPath); 
+}); 
+
+// Exporting the "home" route 
+module.exports = router; 
